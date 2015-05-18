919e3cc9d2037b0ac297503939a9389591567f87
Yonas Robi <Yonas.robi@cgifederal.com>
2015-05-18 16:51:51 -0400

    heroku deployment config updates

--- Gruntfile.js
+++ Gruntfile.js
@@ -449,7 +449,7 @@ module.exports = function(grunt) {
         'sass',
         'useminPrepare',
         'requirejs',
-        'imagemin',
+        //'imagemin',
         'concat',
         'cssmin',
         'uglify',
